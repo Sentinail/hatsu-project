87a2ff8fc5d09f6a0460d115786aca2ce250662a
Sentinail <wilsongponseca@gmail.com>
2023-09-11 13:29:08 +0800

    Used Environment Variables

--- src/firebase-config/firebaseConfig.js
+++ src/firebase-config/firebaseConfig.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD6Vdl_UC3-h0jhMMV1mSG7xQlKaH1gBxY",
-  authDomain: "hatsu-project-ccdf3.firebaseapp.com",
-  projectId: "hatsu-project-ccdf3",
-  storageBucket: "hatsu-project-ccdf3.appspot.com",
-  messagingSenderId: "307123925678",
-  appId: "1:307123925678:web:0200c2ec45e133a791333f"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
--- src/tests/TestApp.js
+++ src/tests/TestApp.js
@@ -1,26 +1,15 @@
 import React, { useContext } from 'react'
 import { TestGlobalStyle } from './TestGlobalStyles'
 import { themeContext } from '../context/themeContext'
-import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'; 
-import { StyledToastContainer } from '../styled-components/StyledToastContainer';
 
 
 const TestApp = () => {
-    const { secondaryColor, tertiaryColor } = useContext(themeContext)
-    const notify = () => {
-      toast('🦄 Wow so easy!');
-    }
-
+    const { secondaryColor } = useContext(themeContext)
 
   return (
     <>
         <TestGlobalStyle $secondaryColor={secondaryColor}></TestGlobalStyle>
-        <button onClick={notify}>Click Me!</button>
-        <StyledToastContainer $tertiaryColor={tertiaryColor} className={"costum_toast"}
-          position="bottom-right"
-          autoClose={10000}
-        />
     </>
   )  
 }
